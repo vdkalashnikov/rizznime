41501bdb3f949687431fc511fb1700fb39e65874
vdkalashnikov <adrfirt@gmail.com>
2024-11-15 22:37:49 +0700

    improve

--- src/components/Main/AnimeList.jsx
+++ src/components/Main/AnimeList.jsx
@@ -1,22 +1,25 @@
-import Anime from "./Anime"
-export default function AnimeList({ title ,animes, onSelectedAnime }) {
-
-    if (!animes || animes.length === 0){
-      return <div id="loading"><div className="loading-wave">
-      <div className="loading-bar"></div>
-      <div className="loading-bar"></div>
-      <div className="loading-bar"></div>
-      <div className="loading-bar"></div>
-    </div></div>
-    }
+import Anime from "./Anime";
+export default function AnimeList({ title, animes, onSelectedAnime }) {
+  if (!animes || animes.length === 0) {
     return (
-      <div className="anime-list">
-        <h2>{title}</h2>
-        <ul className="list list-anime">
-          {animes?.map((anime, index) => (
-            <Anime key={index} anime={anime} onSelectedAnime={onSelectedAnime} />
-          ))}
-        </ul>
+      <div id="loading">
+        <div className="loading-wave">
+          <div className="loading-bar"></div>
+          <div className="loading-bar"></div>
+          <div className="loading-bar"></div>
+          <div className="loading-bar"></div>
+        </div>
       </div>
-    )
+    );
   }
+  return (
+    <div className="anime-list">
+      <h2>{title}</h2>
+      <ul className="list list-anime">
+        {animes?.map((anime, index) => (
+          <Anime key={index} anime={anime} onSelectedAnime={onSelectedAnime} />
+        ))}
+      </ul>
+    </div>
+  );
+}
--- src/components/Main/ModalBox.jsx
+++ src/components/Main/ModalBox.jsx
@@ -1,20 +1,37 @@
 export default function ModalBox({ children }) {
-    return (
-      <div className="modal fade" id="animeDetailModal" tabIndex="-1" aria-labelledby="animeDetailModalLabel" aria-hidden="true">
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="animeDetailModalLabel">Anime Detail</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body">
-              {children}
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-dark" data-bs-dismiss="modal">Close</button>
-            </div>
+  return (
+    <div
+      className="modal fade"
+      id="animeDetailModal"
+      tabIndex="-1"
+      aria-labelledby="animeDetailModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="animeDetailModalLabel">
+              Anime Detail
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">{children}</div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-dark"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
